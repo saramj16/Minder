c58087e0460eeb29578d1fe90fd667386eeaeb92
javoGaig <43567109+javoGaig@users.noreply.github.com>
2019-05-06 10:41:57 +0200

    server connection

--- src/Server/Model/database/DBConnector.java
+++ src/Server/Model/database/DBConnector.java
@@ -24,9 +24,8 @@ public class DBConnector {
 
         public static DBConnector getInstance(){
             if(instance == null){
-                instance = new DBConnector("root", "1234", "minder", 3306);
+                instance = new DBConnector("root", "5432", "minder", 3306);
                 instance.connect();
-
             }
             return instance;
         }

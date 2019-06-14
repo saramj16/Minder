d9c2849879e782d12cc1ad0e0870d3a9cf86ecbb
manellopez2015 <manel.lf@students.salle.url.edu>
2019-06-14 17:05:54 +0200

    test 4

--- src/User/View/View.java
+++ src/User/View/View.java
@@ -237,7 +237,7 @@ public class View extends JFrame {
         return jpMatches;
     }
 
-    //Test3
+    //Test4
     public void autenticationController(ActionListener controller){
         jbMatchYes.addActionListener(controller);
         jbMatchYes.setActionCommand("AcceptUser");
